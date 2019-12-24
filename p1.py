582f7c2de591fd7adf5ff2962defcbd06696ae45
amanzishan <amanzishan.az@gmail.com>
2019-12-24 10:16:49 +0530

    python program

---
+++ p1.py
@@ -0,0 +1,18 @@
+def show_excitement():
+    # Your code goes here
+    l = []
+    string = "I am super excited for this course! "
+    for i in range (5):
+        l.append(string)
+    return tstring(l)
+def tstring(l):
+    str=" "
+    for j in l:
+        str += j
+    return str
+     
+        
+
+        
+    
+print show_excitement()
